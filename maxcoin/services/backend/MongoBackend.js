3b66b7bd6f7efc76a5b68936de951143310423f3
Doyun <materokatti@gmail.com>
2024-09-11 08:52:24 +0900

    ADD : Insert data.json into MongoDB

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -28,7 +28,17 @@ class MongoBackend {
     return false;
   }
 
-  async insert() {}
+  async insert() {
+    const data = await this.coinAPI.fetch();
+    const documents = [];
+    Object.entries(data.bpi).forEach((entry) => {
+      documents.push({
+        date: entry[0],
+        value: entry[1]
+      });
+    });
+    return this.collection.insertMany(documents);
+  }
 
   async getMax() {}
 
@@ -42,6 +52,13 @@ class MongoBackend {
       throw new Error("Connecting to MongoDB failed");
     }
     console.timeEnd("mongodb-connect");
+
+    console.info("Inserting data into MongoDB");
+    console.time("mongodb-insert");
+    const insertResult = await this.insert();
+    console.timeEnd("mongodb-insert");
+
+    console.info(`Inserted ${insertResult.insertedCount} documents into MongoDB`);
 
     console.info("Disconnecting from MongoDB");
     console.time("mongodb-disconnect");
